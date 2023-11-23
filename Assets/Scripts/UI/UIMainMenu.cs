ebe027bae8b90e1311b6445a715e5619a541929b
fishillness <fishillness@gmail.com>
2023-11-23 18:01:16 +0600

    The menu has been changed. Added new panels and the ability to switch between them.

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMainMenu : MonoBehaviour
+namespace Journey
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UIMainMenu : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject MenuPanel;
+        [SerializeField] private GameObject LevelsPanel;
+        [SerializeField] private GameObject SettingsPanel;
+        [SerializeField] private GameObject ControlPanel;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        private void Start()
+        {
+            OpenPanel(MenuPanel);
+
+        }
+
+        public void MenuButton()
+        {
+            OpenPanel(MenuPanel);
+        }
+
+        public void LevelsButton()
+        {
+            OpenPanel(LevelsPanel);
+        }
+
+        public void SettingsButton()
+        {
+            OpenPanel(SettingsPanel);
+        }
+
+        public void ControlButton()
+        {
+            OpenPanel(ControlPanel);
+        }
+
+        public void QuitButton()
+        {
+            Application.Quit();
+        }
+
+        private void OpenPanel(GameObject panel)
+        {
+            MenuPanel.SetActive(false);
+            LevelsPanel.SetActive(false);
+            SettingsPanel.SetActive(false);
+            ControlPanel.SetActive(false);
+
+            panel.SetActive(true);
+        }
+
     }
 }
+
