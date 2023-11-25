33a924975f2ba5a6707cf5c70001493ed434be8c
fishillness <fishillness@gmail.com>
2023-11-25 23:40:32 +0600

    The portal now makes a sound when interacted with.
    Script "Portal" updated.

--- Assets/Scripts/Interacted/Portal.cs
+++ Assets/Scripts/Interacted/Portal.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Journey
@@ -5,9 +6,16 @@ namespace Journey
     public class Portal : TriggerColliderInteracted
     {
         public static event UnityAction OnCompletedLevel;
+        [SerializeField] private SoundPlayer soundPlayer;
+        [SerializeField] private SoundType soundType;
 
         protected override void ActionUponInteraction()
         {
+            if (soundPlayer)
+            {
+                soundPlayer.Play(soundType);
+            }
+
             OnCompletedLevel?.Invoke();
         }
     }
